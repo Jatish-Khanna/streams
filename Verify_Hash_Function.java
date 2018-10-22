e0ee40ff8b2ea9425e55fa438296c04d63719d63
Jatish Khanna <jatish.khanna@gmail.com>
2018-10-22 10:06:01 +0200

    Create Verify_Hash_Function.java
    A hash function is any function that can be used to map data of arbitrary size to data of a fixed size. The values returned by a hash function are called hash values, hash codes, digests, or simply hashes.

---
+++ Verify_Hash_Function.java
@@ -0,0 +1,24 @@
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Test{
+	
+	public static void main(String[] args) {
+		Map<Integer, Long> m = IntStream.range(1,100000)
+			.map(Test::hash)
+			.map(element -> 15 & element)
+			.boxed()
+			.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		m.entrySet().stream()
+			.forEach(element -> System.out.println(element.getKey() + " " + element.getValue()));
+	}
+	
+	static final int hash(Object key) {
+		int calculatedHash = Objects.hashCode(key);
+//		return (calculatedHash ^ Integer.MAX_VALUE) + calculatedHash;
+		return (key == null) ? 0 : (calculatedHash = key.hashCode()) ^ (calculatedHash >>> 16);
+	}
+}
