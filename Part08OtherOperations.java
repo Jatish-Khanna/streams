89e5063c420263ed5813f179ed27bedb5620fad7
Jatish Khanna <jatish.khanna@gmail.com>
2018-11-18 08:41:01 +0100

    Create Part08OtherOperations.java
    In the first exercise we'll receive 3 Flux<String>. Their elements could arrive with latency, yet each time the three sequences have all emitted an element, we want to combine these 3 elements and create a new User. This concatenate-and-transform operation is called zip:
    
    Zip Operator
    
    Success!
    If you have 3 possible Mono sources and you only want to keep the fastest one, you can use the first static method:
    
    Fatest Mono
    
    Success!
    For Flux, a similar result can be achieved using the firstEmitting static method. In this case it's the flux which emits an initial element first which is selected. Flux aren't mixed.
    
    First Emiting
    
    Success!
    Sometimes you're not interested in elements of a Flux<T>. If you want to still keep a Flux<T> type, you can use ignoreElements(). But if you really just want the completion, represented as a Mono<Void>, you can use then() instead:
    
    Flux Completion
    
    Success!
    Reactive Streams does not allow null values in onNext. There's an operator that allow to just emit one value, unless it is null in which case it will revert to an empty Mono. Can you find it?
    
    Null aware user to mono
    
    Success!
    Similarly, if you want to prevent the empty Mono case by falling back to a different one, you can find an operator that does this switch:

---
+++ Part08OtherOperations.java
@@ -0,0 +1,145 @@
+import java.util.*;
+import java.util.function.*;
+import java.time.*;
+
+import io.pivotal.literx.domain.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class Part08OtherOperations {
+
+//========================================================================================
+
+	// TODO Create a Flux of user from Flux of username, firstname and lastname.
+	Flux<User> userFluxFromStringFlux(Flux<String> usernameFlux, Flux<String> firstnameFlux, Flux<String> lastnameFlux) {
+		return Flux.zip(usernameFlux, firstnameFlux, lastnameFlux)
+		            .map(tuple -> new User(tuple.getT1(),
+		                                   tuple.getT2(),
+		                                   tuple.getT3()));
+	}
+	
+	@Test
+	public void zipFirstNameAndLastName() {
+
+		Flux<String> usernameFlux = Flux.just(User.SKYLER.getUsername(), User.JESSE.getUsername(), User.WALTER.getUsername(), User.SAUL.getUsername());
+		Flux<String> firstnameFlux = Flux.just(User.SKYLER.getFirstname(), User.JESSE.getFirstname(), User.WALTER.getFirstname(), User.SAUL.getFirstname());
+		Flux<String> lastnameFlux = Flux.just(User.SKYLER.getLastname(), User.JESSE.getLastname(), User.WALTER.getLastname(), User.SAUL.getLastname());
+
+		Flux<User> userFlux = userFluxFromStringFlux(usernameFlux, firstnameFlux, lastnameFlux);
+		StepVerifier.create(userFlux)
+			.expectNext(User.SKYLER, User.JESSE, User.WALTER, User.SAUL)
+			.expectComplete()
+			.verify();
+	}
+
+//========================================================================================
+
+	// TODO Return the mono which returns its value faster
+	Mono<User> useFastestMono(Mono<User> mono1, Mono<User> mono2) {
+		return Mono.first(mono1, mono2);
+	}
+	
+	@Test
+	public void fastestMono() {
+		ReactiveRepository<User> repository1 = new ReactiveUserRepository(MARIE);
+		ReactiveRepository<User> repository2 = new ReactiveUserRepository(250, MIKE);
+
+		Mono<User> mono = useFastestMono(repository1.findFirst(), repository2.findFirst());
+		StepVerifier.create(mono)
+			.expectNext(MARIE)
+			.expectComplete()
+			.verify();
+
+		repository1 = new ReactiveUserRepository(250, MARIE);
+		repository2 = new ReactiveUserRepository(MIKE);
+		mono = useFastestMono(repository1.findFirst(), repository2.findFirst());
+		StepVerifier.create(mono)
+			.expectNext(MIKE)
+			.expectComplete()
+			.verify();
+	}
+
+//========================================================================================
+
+	// TODO Return the flux which returns the first value faster
+	Flux<User> useFastestFlux(Flux<User> flux1, Flux<User> flux2) {
+		return Flux.firstEmitting(flux1, flux2);
+	}
+	
+	@Test
+	public void fastestFlux() {
+		ReactiveRepository<User> repository1 = new ReactiveUserRepository(MARIE, MIKE);
+		ReactiveRepository<User> repository2 = new ReactiveUserRepository(250);
+		Flux<User> flux = useFastestFlux(repository1.findAll(), repository2.findAll());
+		StepVerifier.create(flux)
+			.expectNext(MARIE, MIKE)
+			.expectComplete()
+			.verify();
+
+		repository1 = new ReactiveUserRepository(250, MARIE, MIKE);
+		repository2 = new ReactiveUserRepository();
+		flux = useFastestFlux(repository1.findAll(), repository2.findAll());
+		StepVerifier.create(flux)
+			.expectNext(User.SKYLER, User.JESSE, User.WALTER, User.SAUL)
+			.expectComplete()
+			.verify();
+	}
+
+//========================================================================================
+
+	// TODO Convert the input Flux<User> to a Mono<Void> that represents the complete signal of the flux
+	Mono<Void> fluxCompletion(Flux<User> flux) {
+		return flux.then();
+	}
+
+	@Test
+	public void complete() {
+		ReactiveRepository<User> repository = new ReactiveUserRepository();
+		Mono<Void> completion = fluxCompletion(repository.findAll());
+		StepVerifier.create(completion)
+			.expectComplete()
+			.verify();
+	}	
+	
+//========================================================================================
+
+	// TODO Return a valid Mono of user for null input and non null input user (hint: Reactive Streams do not accept null values)
+	Mono<User> nullAwareUserToMono(User user) {
+		return Mono.justOrEmpty(user);
+	}
+
+	@Test
+	public void nullHandling() {
+		Mono<User> mono = nullAwareUserToMono(User.SKYLER);
+		StepVerifier.create(mono)
+			.expectNext(User.SKYLER)
+			.expectComplete()
+			.verify();
+
+		mono = nullAwareUserToMono(null);
+		StepVerifier.create(mono)
+			.expectComplete()
+			.verify();
+	}
+
+//========================================================================================
+
+	// TODO Return the same mono passed as input parameter, expect that it will emit User.SKYLER when empty
+	Mono<User> emptyToSkyler(Mono<User> mono) {
+		return mono.defaultIfEmpty(User.SKYLER);
+	}
+	
+	@Test
+	public void emptyHandling() {
+		Mono<User> mono = emptyToSkyler(Mono.just(User.WALTER));
+		StepVerifier.create(mono)
+			.expectNext(User.WALTER)
+			.expectComplete()
+			.verify();
+		mono = emptyToSkyler(Mono.empty());
+		StepVerifier.create(mono)
+			.expectNext(User.SKYLER)
+			.expectComplete()
+			.verify();
+	}
+  }
