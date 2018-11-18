309ccc3aaa162d6571f23f421f7d46ef4bcce228
Jatish Khanna <jatish.khanna@gmail.com>
2018-11-18 08:09:16 +0100

    Create Part09Adapt.java
    You can make RxJava2 and Reactor 3 types interact without a single external library.
    
    In the first two examples we will adapt from Flux to Flowable, which implements Publisher, and vice-versa.
    
    This is straightforward as both libraries provide a factory method to do that conversion from any Publisher. The checker below runs the two opposite conversions in one go:
    
    Flux to Flowable back to Flux
    
    Success!
    The next two examples are a little trickier: we need to adapt between Flux and Observable, but the later doesn't implement Publisher.
    
    In the first case, you can transform any publisher to Observable. In the second case, you have to first transform the Observable into a Flowable, which forces you to define a strategy to deal with backpressure (RxJava 2 Observable doesn't support backpressure).
    
    Flux to Observable and back to Flux
    
    Success!
    Next, let's try to transform a Mono to a RxJava Single, and vice-versa. You can simply call the firstOrError method from Observable. For the other way around, you'll once again need to transform the Single into a Flowable first.
    
    Mono to Single and back to Mono
    
    Success!
    Finally, you can easily transform a Mono to a Java 8 CompletableFuture and vice-versa. Notice how these conversion methods all begin with from (when converting an external type to a Reactor one) and to (when converting a Reactor type to an external one).

---
+++ Part09Adapt.java
@@ -0,0 +1,119 @@
+import java.util.*;
+import java.util.function.*;
+import java.time.*;
+
+import java.util.concurrent.CompletableFuture;
+
+import io.pivotal.literx.domain.User;
+import io.pivotal.literx.repository.ReactiveRepository;
+import io.pivotal.literx.repository.ReactiveUserRepository;
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import io.reactivex.BackpressureStrategy;
+
+/**
+ * Learn how to adapt from/to RxJava 2 Observable/Single/Flowable and Java 8+ CompletableFuture.
+ *
+ * Mono and Flux already implements Reactive Streams interfaces so they are natively
+ * Reactive Streams compliant + there are {@link Mono#from(Publisher)} and {@link Flux#from(Publisher)}
+ * factory methods.
+ *
+ * For RxJava 2, you should not use Reactor Adapter but only RxJava 2 and Reactor Core.
+ *
+ */
+public class Part09Adapt {
+
+	ReactiveRepository<User> repository = new ReactiveUserRepository();
+
+//========================================================================================
+
+	// TODO Adapt Flux to RxJava Flowable
+	Flowable<User> fromFluxToFlowable(Flux<User> flux) {
+		return Flowable.fromPublisher(flux);
+	}
+
+	// TODO Adapt RxJava Flowable to Flux
+	Flux<User> fromFlowableToFlux(Flowable<User> flowable) {
+		return Flux.from(flowable);
+	}
+
+	@Test
+	public void adaptToFlowable() {
+		Flux<User> flux = repository.findAll();
+		Flowable<User> observable = fromFluxToFlowable(flux);
+		StepVerifier.create(fromFlowableToFlux(observable))
+			.expectNext(User.SKYLER, User.JESSE, User.WALTER, User.SAUL)
+			.expectComplete()
+			.verify();
+	}
+
+//========================================================================================
+
+	// TODO Adapt Flux to RxJava Observable
+	Observable<User> fromFluxToObservable(Flux<User> flux) {
+		return Observable.fromPublisher(flux);
+	}
+
+	// TODO Adapt RxJava Observable to Flux
+	Flux<User> fromObservableToFlux(Observable<User> observable) {
+		return Flux.from(observable.toFlowable(BackpressureStrategy.LATEST));
+	}
+
+	@Test
+	public void adaptToObservable() {
+		Flux<User> flux = repository.findAll();
+		Observable<User> observable = fromFluxToObservable(flux);
+		StepVerifier.create(fromObservableToFlux(observable))
+			.expectNext(User.SKYLER, User.JESSE, User.WALTER, User.SAUL)
+			.expectComplete()
+			.verify();
+	}
+	
+//========================================================================================
+
+	// TODO Adapt Mono to RxJava Single
+	Single<User> fromMonoToSingle(Mono<User> mono) {
+		return Single.fromPublisher(mono);
+	}
+
+	// TODO Adapt RxJava Single to Mono
+	Mono<User> fromSingleToMono(Single<User> single) {
+		return Mono.from(single.toFlowable());
+	}
+	
+	@Test
+	public void adaptToSingle() {
+		Mono<User> mono = repository.findFirst();
+		Single<User> single = fromMonoToSingle(mono);
+		StepVerifier.create(fromSingleToMono(single))
+			.expectNext(User.SKYLER)
+			.expectComplete()
+			.verify();
+	}
+
+//========================================================================================
+
+	// TODO Adapt Mono to Java 8+ CompletableFuture
+	CompletableFuture<User> fromMonoToCompletableFuture(Mono<User> mono) {
+		return mono.toFuture();
+	}
+
+	// TODO Adapt Java 8+ CompletableFuture to Mono
+	Mono<User> fromCompletableFutureToMono(CompletableFuture<User> future) {
+		return Mono.fromFuture(future);
+	}
+
+	@Test
+	public void adaptToCompletableFuture() {
+		Mono<User> mono = repository.findFirst();
+		CompletableFuture<User> future = fromMonoToCompletableFuture(mono);
+		StepVerifier.create(fromCompletableFutureToMono(future))
+			.expectNext(User.SKYLER)
+			.expectComplete()
+			.verify();
+	}
+}
